200964abbfac7be0cfd0412d9fde37050466af83
indraadp <indraproject777@gmail.com>
2023-10-04 19:49:33 +0700

    Compress CSS & Js library
    Signed-off-by: indraadp <indraproject777@gmail.com>

--- assets/js/smooth-scroll.js
+++ assets/js/smooth-scroll.js
@@ -1,6 +1,1 @@
-$(document).ready(function () {
-  $("a[href*=#]").click(function (event) {
-    event.preventDefault();
-    $("html,body").animate({ scrollTop: $(this.hash).offset().top }, 500);
-  });
-});
+$(document).ready(function(){$("a[href*=#]").click(function(a){a.preventDefault();$("html,body").animate({scrollTop:$(this.hash).offset().top},500)})});
