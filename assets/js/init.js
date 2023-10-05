3be954983c86eaaeabe5c98be144660e01d87301
indraadp <indraproject777@gmail.com>
2023-10-05 23:20:13 +0700

    Improve layout
    Signed-off-by: indraadp <indraproject777@gmail.com>

---
+++ assets/js/init.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 2800,
+    autoplayHoverPause: true,
+    nav: true,
+    responsiveClass: true,
+    responsive: { 300: { items: 1 }, 600: { items: 2 }, 1000: { items: 3 } },
+    navText: [
+      "<i class='fa-solid fa-circle-chevron-left'></i>",
+      "<i class='fa-solid fa-circle-chevron-right'></i>",
+    ],
+  });
+  AOS.init();
+});
