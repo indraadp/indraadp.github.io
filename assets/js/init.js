6f8699850dd1d377ec4325cfc1ca55f42d739bd9
indraadp <indraproject777@gmail.com>
2023-10-07 12:18:21 +0700

    init.js: Increase carousel autoplayTimeout
    Signed-off-by: indraadp <indraproject777@gmail.com>

--- assets/js/init.js
+++ assets/js/init.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
   $(".owl-carousel").owlCarousel({
     loop: true,
     autoplay: true,
-    autoplayTimeout: 2800,
+    autoplayTimeout: 3000,
     autoplayHoverPause: true,
     nav: true,
     responsiveClass: true,
