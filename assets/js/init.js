dcff4480fb16c565582ec166ad58e3faba0ee9f4
indraadp <indraproject777@gmail.com>
2023-10-07 13:15:21 +0700

    Disable carousel autoplay
    Signed-off-by: indraadp <indraproject777@gmail.com>

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,9 +1,6 @@
 $(document).ready(function () {
   $(".owl-carousel").owlCarousel({
     loop: true,
-    autoplay: true,
-    autoplayTimeout: 3000,
-    autoplayHoverPause: true,
     nav: true,
     responsiveClass: true,
     responsive: { 300: { items: 1 }, 600: { items: 2 }, 1000: { items: 3 } },
