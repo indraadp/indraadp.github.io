7c9e914de7cda5297d887d7a03f206183250787a
indraadp <indraproject777@gmail.com>
2024-03-02 14:19:24 +0700

    Revert "Disable carousel autoplay"
    This reverts commit dcff4480fb16c565582ec166ad58e3faba0ee9f4.

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,6 +1,9 @@
 $(document).ready(function () {
   $(".owl-carousel").owlCarousel({
     loop: true,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true,
     nav: true,
     responsiveClass: true,
     responsive: { 300: { items: 1 }, 600: { items: 2 }, 1000: { items: 3 } },
