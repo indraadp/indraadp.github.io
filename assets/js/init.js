d540cae4d96a8455a1b0fc6bc005885772eef498
indraadp <indraproject777@gmail.com>
2024-03-13 23:20:50 +0700

    Drop featured posts section
    Signed-off-by: indraadp <indraproject777@gmail.com>

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,18 +1,3 @@
-$(document).ready(function () {
-  $(".owl-carousel").owlCarousel({
-    loop: true,
-    autoplay: true,
-    autoplayTimeout: 3500,
-    autoplayHoverPause: true,
-    nav: true,
-    responsiveClass: true,
-    responsive: { 300: { items: 1 }, 600: { items: 2 }, 1000: { items: 3 } },
-    navText: [
-      "<i class='fa-solid fa-circle-chevron-left'></i>",
-      "<i class='fa-solid fa-circle-chevron-right'></i>",
-    ],
-  });
-  AOS.init({
-    once: true,
-  });
+AOS.init({
+  once: true,
 });
